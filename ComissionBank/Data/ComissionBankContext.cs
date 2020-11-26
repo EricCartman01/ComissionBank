a016e8064ca0ca1ac86961f26465e9cc04d96489
EricCartman01 <leomonte01@gmail.com>
2020-11-26 17:44:20 -0300

    Project Created + 1 Migration

---
+++ ComissionBank/Data/ComissionBankContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ComissionBank.Models;
+
+namespace ComissionBank.Data
+{
+    public class ComissionBankContext : DbContext
+    {
+        public ComissionBankContext (DbContextOptions<ComissionBankContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ComissionBank.Models.Teste> Teste { get; set; }
+    }
+}
